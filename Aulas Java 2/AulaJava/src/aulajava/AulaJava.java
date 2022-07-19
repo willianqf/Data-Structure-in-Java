a491e53a20888e840e5d7f0a6d3a88751429f1e5
Willian Quirino <willianqf1996@gmail.com>
2022-07-19 14:12:53 -0300

    Reload in Class

--- Aulas Java 2/AulaJava/src/aulajava/AulaJava.java
+++ Aulas Java 2/AulaJava/src/aulajava/AulaJava.java
@@ -1,9 +1,11 @@
 package aulajava;
 
-
+import java.util.ArrayList;
 
 public class AulaJava {
     public static void main(String[] args) {
-        
+        ListaOrdenada lista = new ListaOrdenada();
+
+        System.out.println("");
     }
 }
---
+++ Aulas Java 2/AulaJava/src/aulajava/ListaOrdenada.java
@@ -0,0 +1,14 @@
+package aulajava;
+
+// Classe para Listas Ordenadas
+public class ListaOrdenada {
+    //Atributos
+    //(visibilidade) (tipo) (nome da variavel)
+    
+
+    //Métodos Construtores
+
+
+    //Métodos
+
+}
